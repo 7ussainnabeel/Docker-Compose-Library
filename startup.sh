f56277c1b672361180487bb53ddf4d04bb51cf5e
hnabeel <7ussainnabeel>
2024-06-12 23:14:47 +0330

    Bash File Update

--- startup.sh
+++ startup.sh
@@ -1,22 +1,13 @@
-!/bin/bash
- Working progress.
- Integrate more interactive choices:
- what would you like to do? 1. launch a server 2. delete a container 3. example, etc. 
-
-while :
-do
-
-# ASCII art header 
-    echo """ 
-  _    _                                                    _       _     
- | |  | |                                                  | |     | |    
- | |__| | ___  _   _ ___  ___  ___  ___ _ ____   _____ _ __| | __ _| |__  
- |  __  |/ _ \| | | / __|/ _ \/ __|/ _ \ '__\ \ / / _ \ '__| |/ _` | '_ \ 
- | |  | | (_) | |_| \__ \  __/\__ \  __/ |   \ V /  __/ |  | | (_| | |_) |
- |_|  |_|\___/ \__,_|___/\___||___/\___|_|    \_/ \___|_|  |_|\__,_|_.__/                                                                                                                      
-    """
-
 #!/bin/bash
+# # ASCII art header 
+# echo " 
+#   _    _                                                    _       _     
+#  | |  | |                                                  | |     | |    
+#  | |__| | ___  _   _ ___  ___  ___  ___ _ ____   _____ _ __| | __ _| |__  
+#  |  __  |/ _ \| | | / __|/ _ \/ __|/ _ \ '__\ \ / / _ \ '__| |/ _` | '_ \ 
+#  | |  | | (_) | |_| \__ \  __/\__ \  __/ |   \ V /  __/ |  | | (_| | |_) |
+#  |_|  |_|\___/ \__,_|___/\___||___/\___|_|    \_/ \___|_|  |_|\__,_|_.__/                                                                                                                      
+# "
 
 # Prompt user to choose a folder
 echo "Choose a folder to cd into:"
